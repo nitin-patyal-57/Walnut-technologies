0c74144f901079c7c5bf62088936366699de200d
Nitin Patyal <nitinpatyal57@gmail.com>
2026-08-11 15:29:05 +0530

    Fix neuro rehab home page image to neuro.jpeg

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -242,7 +242,7 @@ function DivisionsPreview() {
       title: t('divisions.robotics'),
       color: '#7c3aed',
       icon: FiActivity,
-      image: '/walklab 3.0.jpeg',
+      image: '/neuro.jpeg',
       features: t('divisions.roboticsFeatures'),
       link: '/solutions?category=Neuro Rehab Devices',
     },
