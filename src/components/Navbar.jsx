2cba64c0cba3e1fd03995bd970f4e74c80c28439
Nitin Patyal <nitinpatyal57@gmail.com>
2026-09-01 12:37:21 +0530

    fix: adjust logo size to h-10 md:h-12 in header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -96,7 +96,7 @@ export default function Navbar({ onOpenQuote, onOpenSchedule }) {
               <img
                 src="/walnut-logo/walnut_technologies_logo.svg"
                 alt="Walnut Technologies"
-                className="h-14 md:h-16 w-auto object-contain -my-3"
+                className="h-10 md:h-12 w-auto object-contain"
               />
             </a>
 
