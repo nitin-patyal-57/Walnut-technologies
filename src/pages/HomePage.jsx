4824a24539ad236ffed3ecbddd25e05d317f9b8c
Nitin Patyal <nitinpatyal57@gmail.com>
2026-09-21 11:22:26 +0530

    Remove duplicate StatsSection after Hero - stats are now embedded in Hero component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -659,7 +659,6 @@ export default function HomePage({ onOpenSchedule }) {
         keywords="OEM, ODM, electronics manufacturer, medical devices, payment systems, POS terminals, oxygen concentrators, PCB design, SMT assembly, India"
       />
       <Hero onOpenSchedule={onOpenSchedule} />
-      <StatsSection />
       <DivisionsPreview />
       <JourneySection />
       <StatsSection />
